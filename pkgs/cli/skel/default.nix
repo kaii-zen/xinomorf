1912cf38a55acb89b34df506b3a6909bec988b7a
Shay Bergmann <shaybergmann@gmail.com>
2018-06-04 11:13:36 -0400

    remove extraneous blank line

--- pkgs/cli/skel/default.nix
+++ pkgs/cli/skel/default.nix
@@ -1,7 +1,6 @@
 { pkgs     ? import <nixpkgs> {}
 , xinomorf ? import ((import ./xinomorf.nix) + "/lib/wrap-deployment.nix") { inherit pkgs; }}:
 
-
 xinomorf {
   name   = builtins.baseNameOf (builtins.getEnv "PWD");
   src    = ./.;
