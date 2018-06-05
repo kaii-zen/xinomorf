c2112dfb78a17cddfa16004be96d85bc7b6aa072
Shay Bergmann <shaybergmann@gmail.com>
2018-06-04 21:49:17 -0400

    bump cli

--- pkgs/cli/skel/xinomorf.nix
+++ pkgs/cli/skel/xinomorf.nix
@@ -3,6 +3,7 @@
 let
   src = builtins.fetchGit {
     url = https://github.com/kreisys/xinomorf.git;
-    rev = "a9998c4dc379e79a803beee2505efc58f995bee6";
+    rev = "3767c9b4526bd81d8e1c57f191249cb09ca61c74
+";
   };
 in import src {}
