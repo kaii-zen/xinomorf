bb482ec6a80f092b2984b03a5cd3cdffd111e259
Shay Bergmann <shaybergmann@gmail.com>
2018-12-07 18:07:07 -0500

    Improve example

--- test/hello.tf.nix
+++ test/hello.tf.nix
@@ -1,10 +1,19 @@
 { resource, provisioner, ... }:
 
+let pkgs = import <nixpkgs> {}; in
+
 [
   (
-    resource "null_resource" "hello" {} [
+    resource "null_resource" "hello" {
+      triggers = {
+        uuid = "\${uuid()}";
+      };
+    } [
       (provisioner "local-exec" {
-          command = "echo hello world";
+        # Demonstrate creating an ad-hoc script
+        command = pkgs.writeScript "test" ''
+          echo "Hello World!"
+        '';
       })
     ]
   )
