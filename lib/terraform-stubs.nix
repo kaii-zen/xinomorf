6947283dce36610a3ae59075b8f20b0d64f600e5
Shay Bergmann <shaybergmann@gmail.com>
2018-12-16 21:43:57 -0500

    Use attrsets with __toString instead of plain strings
    for some stubs in order to preserve their name and attributes so that
    they can be retrieved later.

--- lib/terraform-stubs.nix
+++ lib/terraform-stubs.nix
@@ -3,17 +3,25 @@
 { vars }:
 
 rec {
-  data = type: name: attrs: ''
+  data = type: name: attrs: {
+    __toString = self: ''
     data ${stringify type} ${stringify name} ${stringify attrs}
   '';
+  };
 
-  resource = type: name: attrs: list: ''
-    resource ${stringify type} ${stringify name} ${stringifyAttrs attrs list}
-  '';
+  resource = type: name: attrs: list: {
+    inherit type name attrs list;
+    __toString = self: ''
+      resource ${stringify type} ${stringify name} ${stringifyAttrs attrs list}
+    '';
+  };
 
-  provider = name: attrs: ''
-    provider ${stringify name} ${stringify attrs}
-  '';
+  provider = name: attrs: {
+    inherit name attrs;
+    __toString = self: ''
+      provider ${stringify name} ${stringify attrs}
+    '';
+  };
 
   provisioner = name: attrs: ''
     provisioner ${stringify name} ${stringify attrs}
@@ -22,9 +30,12 @@ rec {
   module = name: attrs: with lib; let
     getTfvars = path: if (builtins.readDir path) ? "terraform.tfvars.json" then importJSON "${path}/terraform.tfvars.json" else {};
     extraAttrs = if attrs ? source then getTfvars attrs.source else {};
-  in ''
-    module ${stringify name} ${stringify (recursiveUpdate extraAttrs attrs)}
-  '';
+  in {
+    inherit name attrs;
+    __toString = self: ''
+      module ${stringify name} ${stringify (recursiveUpdate extraAttrs attrs)}
+    '';
+  };
 
   locals = attrs: ''
     locals ${stringify attrs}
